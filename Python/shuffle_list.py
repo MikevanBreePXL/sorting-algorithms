00f9687afd6ea65c18d41522a08cfd4fea232357
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-25 01:50:52 +0100

    Fix - var name overlapped & fischer fix implement.

--- Python/shuffle_list.py
+++ Python/shuffle_list.py
@@ -1,19 +1,20 @@
 import random
 # shuffle algorithmns
 
-# Naïve shuffle: go through every element of the list and put it in a random position
-def shuffle_naive(list):
-    for i in range(len(list)):
-        list[random.randint(0, len(list)-1)] = list[i]
+# Naïve shuffle: go through every element of the input_list and put it in a random position
+def shuffle_naive(input_list):
+    for i in range(len(input_list)):
+        input_list[random.randint(0, len(input_list)-1)] = input_list[i]
 
-    return list
+    return input_list
 
-# Fisher-Yates shuffle: Take a random element from the list and add it to a seperate (randomized/result) list
-def shuffle_fischer_yates(list):
+# Fisher-Yates shuffle: Take a random element from the input_list and add it to a seperate (randomized/result) input_list
+def shuffle_fischer_yates(input_list):
     result_list = []
     
-    iteration_length = len(list)
-    for i in range(iteration_length):
-        result_list.append(list[random.randint(0, len(list)-1)])
+    for i in range(len(input_list)):
+        index = random.randint(0, len(input_list)-1)
+        result_list.append(input_list[index])
+        input_list.pop(index)
     
     return result_list
