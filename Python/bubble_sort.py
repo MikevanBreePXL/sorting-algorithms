435d9db456bd1ed0a366fc94c2a86e35aa2e77d3
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-25 06:36:39 +0100

    Fix - simplified code for direction

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -3,53 +3,45 @@ from check_list_sorted import checkListSort
 # bubble sort
 
 def bubble_sort(sort_list, direction = "ascending"):
-    is_sorted = False
+    if direction.lower()[0:4] != "desc" and direction.lower()[0:3] != "asc":
+        raise Exception('direction needs to be default, "ascending" or "descending", your value: ' + direction)
+
     i = 1
     j = 0
     total_comparisons = 0
     list_loops = 1
-    while is_sorted == False:
+    while True:
         # check if swap needed
         #debug/verbose: print(f"Compare: [{sort_list[j]}](L) & [{sort_list[i]}](R)")
-        match direction:
-            case "ascending":
-                if sort_list[i] < sort_list[j]:
-                    # Swap!
-                    #debug/verbose: print("Swap!")
-                    swap_var = sort_list[j]
-                    sort_list[j] = sort_list[i]
-                    sort_list[i] = swap_var
-                total_comparisons += 1
-            case "descending":
-                if sort_list[i] > sort_list[j]:
-                    # Swap!
-                    #debug/verbose: print("Swap!")
-                    swap_var = sort_list[j]
-                    sort_list[j] = sort_list[i]
-                    sort_list[i] = swap_var
-                total_comparisons += 1
-            case _:
-                error = ValueError
-                error.add_note('direction needs to be default, "ascending" or "descending"')
-                raise error
+
+        if sort_list[i] < sort_list[j]:
+            # Swap!
+            swap_var = sort_list[j]
+            sort_list[j] = sort_list[i]
+            sort_list[i] = swap_var
+            #debug/verbose: print(f"Swap!\n=> {sort_list}")
+        total_comparisons += 1
 
         # if not: next bubble
-        #debug/verbose: print(f"Next iteration")
         if i >= len(sort_list) - list_loops:
             # if iterators are at the end of the list
             
             #check if sorted
-            #debug/verbose: print(f"Sorted? {checkListSort(sort_list)}\n{sort_list}")
-            is_sorted = checkListSort(sort_list)
-
-            # if not sorted: loop back around (first bubble)
-            #debug/verbose: print("Looped back to start of the list")
-            i = 1
-            j = 0
-            list_loops += 1
+            #debug/verbose: print(f"Sorted? {checkListSort(sort_list)}")
+            if checkListSort(sort_list):
+                if direction.lower()[0-3] == "des":
+                    sort_list = sort_list.reverse()
+                
+                return sort_list, total_comparisons
+                #debug/verbose: print("--- EXITED SUCCESSFULLY ---")
+            else:
+                # if not sorted: loop back around (first bubble)
+                #debug/verbose: print(f"{sort_list}\nLooped back to start of list")
+                i = 1
+                j = 0
+                list_loops += 1
         else:
             # increment iterators by 1 (next bubble)
+            #debug/verbose: print(f"Next bubble")
             i += 1
             j += 1
-    # if sorted: return the sorted list
-    return sort_list, total_comparisons
--- Python/merge_sort.py
+++ Python/merge_sort.py
@@ -1,60 +1,42 @@
 from  check_list_sorted import checkListSort
 # merge sort (recursive)
 
-# function to split the list in 2 halfs
-def split_list(list):
-    half = len(list)//2
-    #debug/verbose: print(f"Splitting {list} in half => {list[:half]} & {list[half:]}")
-    return list[:half], list[half:]
+def merge_sort(input_list, direction="ascending"):
+    if direction.lower()[0:4] != "desc" and direction.lower()[0:3] != "asc":
+        raise Exception('direction should be default, "ascending" or "descending", your value: ' + direction)
 
-def merge_sort(input_list1, input_list2, direction="ascending"):
-    merge_list1 = input_list1.copy()
-    merge_list2 = input_list2.copy()
+    half = len(input_list)//2
+    input_half1 = input_list[:half]
+    input_half2 = input_list[half:]
+    #debug/verbose: print(f"Splitting {input_list} => {input_half1} & {input_half2}")
+    merge_list1 = input_half1.copy()
+    merge_list2 = input_half2.copy()
     comparison_counter = 0
 
     # check and split until sequence size 1 and work back up the stack (recursive)
     if len(merge_list1) > 1:
-        a, b = split_list(merge_list1)
-        merge_list1, comparisons_done = merge_sort(a, b, direction)
+        merge_list1, comparisons_done = merge_sort(merge_list1, direction)
         comparison_counter += comparisons_done
     if len(merge_list2) > 1:
-        a, b = split_list(merge_list2)
-        merge_list2, comparisons_done = merge_sort(a, b, direction)
+        merge_list2, comparisons_done = merge_sort(merge_list2, direction)
         comparison_counter += comparisons_done
+    
 
     # logic part
     #debug/verbose: print(f"Merging {merge_list1} & {merge_list2} next")
-    range_size = max(len(merge_list1), len(merge_list2))
     result_list = []
     # continue until a list is empty
     while True:
         # take lowest element and put it in the result list
-        match direction:
-            case "ascending":
-                comparison_counter += 1
-                if (merge_list1[0] < merge_list2[0]):
-                    #debug/verbose: print(f"Adding [{merge_list1[0]}] over [{merge_list2[0]}]")
-                    result_list.append(merge_list1[0])
-                    merge_list1.pop(0)
-                else:
-                    #debug/verbose: print(f"Adding [{merge_list2[0]}] over [{merge_list1[0]}]")
-                    result_list.append(merge_list2[0])
-                    merge_list2.pop(0)
-                
-            case "descending":
-                comparison_counter += 1
-                if (merge_list1[0] > merge_list2[0]):
-                    result_list.append(merge_list1[0])
-                    #debug/verbose: print(f"Adding [{merge_list1[0]}] over [{merge_list2[0]}] => {result_list}")
-                    merge_list1.pop(0)
-                else:
-                    result_list.append(merge_list2[0])
-                    #debug/verbose: print(f"Adding [{merge_list2[0]}] over [{merge_list1[0]}] => {result_list}")
-                    merge_list2.pop(0)
-            case _:
-                error = ValueError()
-                error.add_note('direction needs to be empty, "ascending" or "descending"')
-                raise error
+        comparison_counter += 1
+        if (merge_list1[0] < merge_list2[0]):
+            result_list.append(merge_list1[0])
+            #debug/verbose: print(f"Adding [{merge_list1[0]}] over [{merge_list2[0]}] => {result_list}")
+            merge_list1.pop(0)
+        else:
+            result_list.append(merge_list2[0])
+            #debug/verbose: print(f"Adding [{merge_list2[0]}] over [{merge_list1[0]}] => {result_list}")
+            merge_list2.pop(0)
 
         # if some list is empty (range takes longest size), end the loop
         if len(merge_list1) == 0 or len(merge_list2) == 0:
@@ -71,6 +53,10 @@ def merge_sort(input_list1, input_list2, direction="ascending"):
             #debug/verbose: print(f"Appending [{merge_list2[i]}]")
             result_list.append(merge_list2[i])
 
+    # flip the list if direction is set to descending
+    if (direction.lower()[0:4] == "desc"):
+        result_list = result_list.reverse()
+
     # return the merged list
-    #debug/verbose: print(f"Merged lists: {input_list1} & {input_list2}\n=> {result_list}\n---")
+    #debug/verbose: print(f"Merged lists: {input_half1} & {input_half2}\n=> {result_list}\n---")
     return result_list, comparison_counter
