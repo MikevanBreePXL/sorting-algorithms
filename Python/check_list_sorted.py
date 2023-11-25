8842c7af84b7d61277310e7c098c97bef2eef378
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-25 04:05:54 +0100

    Add - Added directional parameter to check

--- Python/check_list_sorted.py
+++ Python/check_list_sorted.py
@@ -1,10 +1,21 @@
 # checkListSort
 # returns True if the given list is sorted in a descending order
 # returns False if not
-def checkListSort(list):
+def checkListSort(list, direction = "ascending"):
     j = 0
-    for i in range(1, len(list)):
-        if list[i] > list[j]:
-            return False
-        j += 1
-    return True
+    match direction:
+        case "ascending":
+            for i in range(1, len(list)):
+                if list[i] < list[j]:
+                    return False
+                j += 1
+            return True
+        case "descending":
+            for i in range(1, len(list)):
+                if list[i] > list[j]:
+                    return False
+                j += 1
+            return True
+        case _:
+            error = ValueError
+            error.add_note('direction needs to be default, "ascending" or "descending"')
