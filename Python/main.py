828cc85d8f44694316b493f139a6390cee1df0d9
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-25 06:56:29 +0100

    Add - Added demo code for Bubble Insertion & Merge

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,49 @@
 from bubble_sort import bubble_sort
+from merge_sort import merge_sort
+from insertion_sort import insertion_sort
 from shuffle_list import shuffle_fischer_yates
+from shuffle_list import shuffle_naive
 
-sorted_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(f"List size: {len(sorted_list)}\n{sorted_list}\n")
+# scroll all the way down
+# scroll all the way down
 
-shuffled_list = shuffle_fischer_yates(sorted_list)
-print(f"shuffled by Fischer-Yates shuffle:\n{shuffled_list}")
+def shuffle_using_Fischer_Yates(any_list):
+    shuffled_list = shuffle_fischer_yates(any_list)
+    print("List size: "+ len(shuffled_list))
+    print(f"shuffled by Fischer-Yates shuffle:\n{shuffled_list}\n")
+    return shuffled_list
 
-# bubble sort
-bubble_sorted_list, comparisons_taken = bubble_sort(shuffled_list)
-print(f"sorted list by using merge sort:\n{bubble_sorted_list}")
-print(f"Comparisons taken: {comparisons_taken}\n")
+def shuffle_using_naive(any_list):
+    shuffled_list = shuffle_naive(any_list)
+    print("List size: " + len(shuffled_list))
+    print(f"shuffled by Naive shuffle:\n{shuffled_list}\n")
+    return shuffled_list
+
+
+def write_sort_output(original_list, sorted_list, comparison_amount):
+    print(f"Comparisons taken: {comparison_amount}")
+    print("  - " + original_list)
+    print("    -> " + sorted_list)
+
+def sort_using_bubble_sort(unsorted_list, order_direction = "descending"):
+    sorted_list, comparison_amount = bubble_sort(unsorted_list, order_direction)
+    write_sort_output(unsorted_list, sorted_list, comparison_amount)
+
+def sort_using_insertion_sort(unsorted_list, order_direction = "descending"):
+    sorted_list, comparison_amount = insertion_sort(unsorted_list, order_direction)
+    write_sort_output(unsorted_list, sorted_list, comparison_amount)
+
+def sort_using_merge_sort(unsorted_list, order_direction = "descending"):
+    sorted_list, comparison_amount = merge_sort(unsorted_list, order_direction)
+    write_sort_output(unsorted_list, sorted_list, comparison_amount)
+
+
+
+
+list_to_shuffle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+shuffled_list = shuffle_using_Fischer_Yates(list_to_shuffle)
+# shuffled_list = shuffle_using_naive(list_to_shuffle)
+
+sort_using_bubble_sort(shuffled_list)
+# sort_using_insertion_sort(shuffled_list)
+# sort_using_merge_sort(shuffled_list)
