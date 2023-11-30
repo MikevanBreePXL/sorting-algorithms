614a187987e51ed5cb21709169108d7cfd124316
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-30 14:36:38 +0100

    Change - Fixed file up for regular function usage

--- Python/quick_sort.py
+++ Python/quick_sort.py
@@ -3,7 +3,7 @@ import random
 def quick_sort_pivot_last(unsorted_list):
     if len(unsorted_list) < 2:
         return unsorted_list
-    print(f"Going to quick sort list: {unsorted_list}")
+    #debug/verbose: print(f"Going to quick sort list: {unsorted_list}")
     
     # Create variables for iterating through the list
     pivot_index = len(unsorted_list) - 1
@@ -28,12 +28,8 @@ def quick_sort_pivot_last(unsorted_list):
     unsorted_list[pivot_index] = swap_temp
     pivot_index = i_pointer
 
-    print(f"- part 1: {unsorted_list[:pivot_index]}")
-    print(f"- pivot (correct place): {unsorted_list[pivot_index]}")
-    print(f"- part 2: {unsorted_list[pivot_index + 1:]}")
-    print(f"New list: {unsorted_list}")
+    #debug/verbose: print(f"- part 1: {unsorted_list[:pivot_index]}")
+    #debug/verbose: print(f"- pivot (correct place): {unsorted_list[pivot_index]}")
+    #debug/verbose: print(f"- part 2: {unsorted_list[pivot_index + 1:]}")
+    #debug/verbose: print(f"New list: {unsorted_list}")
     return quick_sort_pivot_last(unsorted_list[:pivot_index]) + [unsorted_list[pivot_index]] + quick_sort_pivot_last(unsorted_list[pivot_index + 1:])
-
-to_sort_list = [11, 2, 8, 4, 10, 7, 1, 3, 9, 6, 5]
-print(f"List to sort: {to_sort_list}\n")
-print(f"\nFinal list: {quick_sort_pivot_last(to_sort_list)}")
