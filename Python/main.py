3452e3cfbd59a92fa4e4b4bf19b3363333bd3c3a
MikevanBreePXL <Mike.vanBree@student.pxl.be>
2023-11-30 15:04:04 +0100

    Add, Change... Add - testcode for QuickSort Change - Made string prints consistent + comp. with Python 3.12

--- Python/main.py
+++ Python/main.py
@@ -3,27 +3,28 @@ from merge_sort import merge_sort
 from insertion_sort import insertion_sort
 from shuffle_list import shuffle_fischer_yates
 from shuffle_list import shuffle_naive
+from quick_sort import quick_sort_pivot_last
 
 # scroll all the way down
 # scroll all the way down
 
 def shuffle_using_Fischer_Yates(any_list):
     shuffled_list = shuffle_fischer_yates(any_list)
-    print("List size: "+ len(shuffled_list))
+    print(f"List size: {len(shuffled_list)}")
     print(f"shuffled by Fischer-Yates shuffle:\n{shuffled_list}\n")
     return shuffled_list
 
 def shuffle_using_naive(any_list):
     shuffled_list = shuffle_naive(any_list)
-    print("List size: " + len(shuffled_list))
+    print(f"List size: {len(shuffled_list)}")
     print(f"shuffled by Naive shuffle:\n{shuffled_list}\n")
     return shuffled_list
 
 
 def write_sort_output(original_list, sorted_list, comparison_amount):
     print(f"Comparisons taken: {comparison_amount}")
-    print("  - " + original_list)
-    print("    -> " + sorted_list)
+    print(f"  - {original_list}")
+    print(f"    -> {sorted_list}")
 
 def sort_using_bubble_sort(unsorted_list, order_direction = "descending"):
     sorted_list, comparison_amount = bubble_sort(unsorted_list, order_direction)
@@ -37,13 +38,17 @@ def sort_using_merge_sort(unsorted_list, order_direction = "descending"):
     sorted_list, comparison_amount = merge_sort(unsorted_list, order_direction)
     write_sort_output(unsorted_list, sorted_list, comparison_amount)
 
+def sort_using_quick_sort(unsorted_list, order_direction = "descending"):
+    sorted_list, comparison_amount = quick_sort_pivot_last(unsorted_list, order_direction)
+    write_sort_output(unsorted_list, sorted_list, comparison_amount)
+
 
 
 
 list_to_shuffle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-shuffled_list = shuffle_using_Fischer_Yates(list_to_shuffle)
 # shuffled_list = shuffle_using_naive(list_to_shuffle)
+shuffled_list = shuffle_using_Fischer_Yates(list_to_shuffle)
 
-sort_using_bubble_sort(shuffled_list)
+sort_using_quick_sort(shuffled_list)
 # sort_using_insertion_sort(shuffled_list)
 # sort_using_merge_sort(shuffled_list)
